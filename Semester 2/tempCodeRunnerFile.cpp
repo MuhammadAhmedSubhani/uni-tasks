b701d1661ff5836959ae72f3c75da2e88d7bd015
Muhammad Ahmed Subhani <ahmadsubhani6741@gmail.com>
2025-11-16 17:20:37 +0500

    changes

--- Semester 2/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <curl/curl.h>
-using namespace std;
-
-int main() {
-    curl_version_info_data *data = curl_version_info(CURLVERSION_NOW);
-    cout << "libcurl version: " << data->version << endl;
-    return 0;
-}
