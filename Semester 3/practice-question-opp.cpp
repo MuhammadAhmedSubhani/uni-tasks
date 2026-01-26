396f3091b8e292bf635dd9ad8b7384e566a8ec99
Muhammad Ahmed Subhani <ahmadsubhani6741@gmail.com>
2026-01-26 19:03:36 +0500

    Refactor car class to encapsulate properties and update main function for object instantiation

--- Semester 3/practice-question-opp.cpp
+++ Semester 3/practice-question-opp.cpp
@@ -4,18 +4,29 @@ using namespace std;
 
 //Classes and Objects (the foundation)
 class car {
-    public:
+    private:
         string name;
         int plate;
         int engine;
+    public:
+        car (string nam1, int plate1, int engine1){
+            name = nam1;
+            plate = plate1;
+            engine = engine1;
+            cout << name << endl << plate << endl << engine;
+        }
+
+        // void input(){
+        //     cout << "Enter car name:";
+        //     cin >> name;
+        //     cout << "Enter car plate";
+        //     cin >> plate;
+        //     cout << "Enter car's engine model:" << endl;
+        //     cin >> engine;
+        // }
+    
 };
 int main() {
-    car car;
-    cout << "Enter car's name: " << endl;
-    cin >> car.name;
-    cout << "Enter car's plate: " << endl;
-    cin >> car.plate;
-    cout << "Enter car's engine: " << endl;
-    cin >> car.engine;
-    cout << car.name << endl << car.plate << endl << car.engine;
+    car civic("civic", 1534, 67);
+    
 }
